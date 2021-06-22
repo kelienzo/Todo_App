2e910befbeb1af20b767a9245028983a171da71d
kelienzo <kellyefone@gmail.com>
2021-06-22 14:29:52 +0100

    Update commit

--- app/src/main/java/com/kelly/todoapp/MainActivity.kt
+++ app/src/main/java/com/kelly/todoapp/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.kelly.todoapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-    val userName: String = "Admin"
-    val passWord: String = "Admin"
+    val validUserName: String = "Admin"
+    val validPassWord: String = "Admin"
+
+    //Layout variables
     private lateinit var userNameInput: EditText
     private lateinit var passWordInput: EditText
     private lateinit var submitButton: Button
@@ -15,5 +19,36 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        userNameInput = findViewById(R.id.userNameInput)
+        passWordInput = findViewById(R.id.passWordInput)
+        submitButton = findViewById(R.id.submitButton)
+
+        submitButton.setOnClickListener {
+            val username = userNameInput.text.toString()
+            val password = passWordInput.text.toString()
+
+            login(username, password)
+        }
+    }
+
+    fun login(username: String, password: String) {
+        if (isValidCredentials(username, password)) {
+            val intent: Intent = Intent(this, HomeActivity::class.java)
+            intent.putExtra("username", username)
+            startActivity(intent)
+        } else {
+            Toast.makeText(
+                applicationContext,
+                "You don type wrong details bros",
+                Toast.LENGTH_SHORT
+            )
+                .show()
+        }
+    }
+
+
+    private fun isValidCredentials(username: String, password: String): Boolean {
+        if (username == validUserName && password == validPassWord) return true
+        return false
     }
 }
